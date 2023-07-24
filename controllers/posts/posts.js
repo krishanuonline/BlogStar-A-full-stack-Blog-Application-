f91e67f78385039ae45dcd69d13577950136e76e
KrishanU178 <krishanu178@gmail.com>
2023-07-24 12:30:40 +0530

    create post with image api

--- controllers/posts/posts.js
+++ controllers/posts/posts.js
@@ -1,73 +1,97 @@
+// model import
+const Post = require("../../models/post/Post");
+const User = require("../../models/user/User");
+const appErr = require("../../utils/appErr");
+
 //create post
-const createPostCtrl = async (req,res)=>{
+const createPostCtrl = async (req, res, next) => {
+    const { title, description, catagory, image, user } = req.body;
     try {
+        if (!title || !description || !catagory || req.file) {
+            return next(appErr("All fields are required", 400));
+        }
+        //find the user
+        const userId = req.session.userAuth;
+        const userFound = await User.findById(userId);
+        //create the post
+        const postCreated = await Post.create({
+            title,
+            description,
+            catagory,
+            user: userFound._id,
+            image: req.file.path,
+        });
+        //push the post created into the array of users post
+        userFound.posts.push(postCreated._id);
+        //resave the user
+        await userFound.save()
         res.json({
-            status:"Success",
-            user:"post Created",
+            status: "Success",
+            data: "post Created",
         });
     } catch (error) {
         res.json({
-            status:"Failed",
-            error:error,
+            status: "Failed",
+            error: error,
         })
     }
 };
 //get all post
-const fetchPostCtrls = async (req,res)=>{
+const fetchPostCtrls = async (req, res) => {
     try {
         res.json({
-            status:"Success",
-            user:"posts displayed",
+            status: "Success",
+            user: "posts displayed",
         });
     } catch (error) {
         res.json({
-            status:"Failed",
-            error:error,
+            status: "Failed",
+            error: error,
         })
     }
 
 };
 //get single post
-const fetchSinglePostCtrl = async (req,res)=>{
+const fetchSinglePostCtrl = async (req, res) => {
     try {
         res.json({
-            status:"Success",
-            user:"post details",
+            status: "Success",
+            user: "post details",
         });
     } catch (error) {
         res.json({
-            status:"Failed",
-            error:error,
+            status: "Failed",
+            error: error,
         })
     }
 
 };
 //delete post
-const deletePostCtrl = async (req,res)=>{
+const deletePostCtrl = async (req, res) => {
     try {
         res.json({
-            status:"Success",
-            user:"post deleted",
+            status: "Success",
+            user: "post deleted",
         });
     } catch (error) {
         res.json({
-            status:"Failed",
-            error:error,
+            status: "Failed",
+            error: error,
         })
     }
 
 };
 //edit post
-const updatePostCtrl = async (req,res)=>{
+const updatePostCtrl = async (req, res) => {
     try {
         res.json({
-            status:"Success",
-            user:"post updated",
+            status: "Success",
+            user: "post updated",
         });
     } catch (error) {
         res.json({
-            status:"Failed",
-            error:error,
+            status: "Failed",
+            error: error,
         })
     }
 
--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const postRoutes = express.Router();
-const {createPostCtrl,fetchPostCtrls,fetchSinglePostCtrl,deletePostCtrl,updatePostCtrl} = require("../../controllers/posts/posts");
+const multer = require("multer");
+const { createPostCtrl, fetchPostCtrls, fetchSinglePostCtrl, deletePostCtrl, updatePostCtrl } = require("../../controllers/posts/posts");
+const protected = require("../../middleware/protected");
+const storage = require("../../config/cloudinary");
+//instance of multer
+const upload = multer({
+    storage,
+});
 // Create post
-postRoutes.post("/",createPostCtrl);
+postRoutes.post("/", protected, upload.single("file"), createPostCtrl);
 // GET all posts
-postRoutes.get("/",fetchPostCtrls);
+postRoutes.get("/", fetchPostCtrls);
 // get specific post 
-postRoutes.get("/:id",fetchSinglePostCtrl);
+postRoutes.get("/:id", fetchSinglePostCtrl);
 // DELETE post
-postRoutes.delete("/:id",deletePostCtrl);
+postRoutes.delete("/:id", deletePostCtrl);
 // edit/modify post
-postRoutes.put("/:id",updatePostCtrl);
+postRoutes.put("/:id", updatePostCtrl);
 
 module.exports = postRoutes;
